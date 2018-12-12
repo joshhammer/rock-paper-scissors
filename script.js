9d2b63493da94a7dc95b4a9126a24ef901e85016
Josua Hämmerle <josua.haemmerle@ricardo.ch>
2018-12-12 09:56:37 +0100

    add new functionality with basic UI

--- script.js
+++ script.js
@@ -1,146 +1,141 @@
+let random;
+let result;
+let computerSelection;
+let playerInput;
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
 
+let cpuDisplay = document.querySelector("#cpuDisplay");
+let playerDisplay = document.querySelector("#playerDisplay");
+let displayText = document.querySelector("#displayText"); 
 
-    let random;
-    let result;
-    let computerSelection;
-    let playerInput;
-    let playerSelection;
-    let playerScore = 0;
-    let computerScore = 0;
-    let ties = 0;
+let rockbtn = document.querySelector("#rockbtn")
+let paperbtn = document.querySelector("#paperbtn")
+let scissorsbtn = document.querySelector("#scissorsbtn")
 
-// clicking the START GAME button will execute one complete game
+// clicking the New Game button will reset the game
 let startButton = document.querySelector("#startbtn");
 startButton.addEventListener("click", function () {
-    game();
+    resetGame();
 });
-    
-    
-    // this function randomly generates rock, paper or scissor for the CPU
-    function computerPlay() {
-        let random = Math.floor(Math.random() * 3 + 1);
-        if (random === 1) {
-            return "rock";
-        }
-        else if (random === 2) {
-            return "paper";
-        }
-        else {
-            return "scissors";
-        }
-    }
-    
-    
-    
-    // this function plays one round and stores the result in "result"
-    function playRound(computerSelection, playerSelection) {
-        if (computerSelection == playerSelection) {
-            result = "Tie!";
-        }
-        else if (computerSelection == "rock" && playerSelection == "paper") {
-            result = "Player wins.";
-        }
-        else if (computerSelection == "rock" && playerSelection == "scissors") {
-            result = "CPU wins.";
-        }
-        else if (computerSelection == "scissors" && playerSelection == "paper") {
-            result = "CPU wins.";
-        }
-        else if (computerSelection == "scissors" && playerSelection == "rock") {
-            result = "Player wins.";
-        }
-        else if (computerSelection == "paper" && playerSelection == "rock") {
-            result = "CPU wins.";
-        }
-        else if (computerSelection == "paper" && playerSelection == "scissors") {
-            result = "Player wins.";
-        }
-    
 
-        if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-            result = "Invalid selection";
-        }
-        else if (result == "Player wins.") {
-            playerScore++;
-        }
-        else if (result == "CPU wins.") {
-            computerScore++;
-        }
-        else if (result == "Tie!") {
-            ties++;
-        }
-    }
-    
-    
-    function game() {
-    
-        playerInput = prompt("Rock, Paper or Scissors? Choose your weapon:");
-        if (playerInput == null) {
-            return
-        }
-        while (playerInput === "") {
-            alert("Well, you have to choose something...");
-            playerInput = prompt("Rock, Paper or Scissors? Choose your weapon:")
-        }
-        playerSelection = playerInput.toLowerCase();
-        computerSelection = computerPlay();
-    
-        playRound(computerSelection, playerSelection);
-    
-        while ((playerScore < 5 || computerScore < 5) && playerInput !== "") {
-    
-            console.log("Player: " + playerSelection);
-            console.log("CPU: " + computerSelection);
-            console.log(result);
-            console.log("Player Score: " + playerScore);
-            console.log("CPU Score: " + computerScore);
-            console.log("Ties: " + ties);
-            console.log("");
-    
-            playerInput = prompt("Rock, Paper or Scissors? Choose again:");
-    
-            while (playerInput == null) {
-                playerInput = prompt("You cannot escape! Choose your weapon, coward!:")
-            }
-    
-            while (playerInput === "") {
-                alert("Well, you have to choose something...");
-                playerInput = prompt("Rock, Paper or Scissors? Choose your weapon:")
-            }
-    
-            playerSelection = playerInput.toLowerCase();
-            computerSelection = computerPlay();
-            playRound(computerSelection, playerSelection);
-    
-    
-            if (playerScore == 5) {
-                console.log("Player: " + playerSelection);
-                console.log("CPU: " + computerSelection);
-                console.log(result);
-                console.log("Player Score: " + playerScore);
-                console.log("CPU Score: " + computerScore);
-                return "PLAYER WINS 5 ROUNDS. CONGRATULATIONS!"
-            }
-    
-            else if (computerScore == 5) {
-                console.log("Player: " + playerSelection);
-                console.log("CPU: " + computerSelection);
-                console.log(result);
-                console.log("Player Score: " + playerScore);
-                console.log("CPU Score: " + computerScore);
-                return "CPU WINS 5 ROUNDS. YOU LOSE!"
-            }
-    
-            else if (result == "Invalid selection") {
-                playerInput = prompt("Rock, Paper or Scissors? Choose again:");
-                playerSelection = playerInput.toLowerCase();
-                computerSelection = computerPlay();
-                playRound(computerSelection, playerSelection);
-            }
-        }
-    
-    }
-    
-   
 
-    
+// create three buttons and add event listener that calls playRound with correct playerSelection
+rockbtn.addEventListener("click", function () {
+    playerSelection = "rock";
+    computerSelection = computerPlay();
+    playRound(computerSelection, playerSelection);
+    cpuDisplay.textContent = computerScore;
+    playerDisplay.textContent = playerScore;
+    displayText.textContent = `You chose ${playerSelection}, CPU chose ${computerSelection}. ${result} `
+
+    checkForWinner();
+});
+
+paperbtn.addEventListener("click", function () {
+    playerSelection = "paper";
+    computerSelection = computerPlay();
+    playRound(computerSelection, playerSelection);
+    cpuDisplay.textContent = computerScore;
+    playerDisplay.textContent = playerScore;
+    displayText.textContent = `You chose ${playerSelection}, CPU chose ${computerSelection}. ${result} `
+
+    checkForWinner();
+});
+
+scissorsbtn.addEventListener("click", function () {
+    playerSelection = "scissors";
+    computerSelection = computerPlay();
+    playRound(computerSelection, playerSelection);
+    cpuDisplay.textContent = computerScore;
+    playerDisplay.textContent = playerScore;
+    displayText.textContent = `You chose ${playerSelection}, CPU chose ${computerSelection}. ${result} `
+
+    checkForWinner();
+});
+
+
+
+// this function randomly generates rock, paper or scissor for the CPU
+function computerPlay() {
+    let random = Math.floor(Math.random() * 3 + 1);
+    if (random === 1) {
+        return "rock";
+    }
+    else if (random === 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+// this function plays one round and stores the result in "result"
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection == playerSelection) {
+        result = "Tie!";
+    }
+    else if (computerSelection == "rock" && playerSelection == "paper") {
+        result = "Player wins.";
+    }
+    else if (computerSelection == "rock" && playerSelection == "scissors") {
+        result = "CPU wins.";
+    }
+    else if (computerSelection == "scissors" && playerSelection == "paper") {
+        result = "CPU wins.";
+    }
+    else if (computerSelection == "scissors" && playerSelection == "rock") {
+        result = "Player wins.";
+    }
+    else if (computerSelection == "paper" && playerSelection == "rock") {
+        result = "CPU wins.";
+    }
+    else if (computerSelection == "paper" && playerSelection == "scissors") {
+        result = "Player wins.";
+    }
+
+
+    if (result == "Player wins.") {
+        playerScore++;
+    }
+    else if (result == "CPU wins.") {
+        computerScore++;
+    }
+    else if (result == "Tie!") {
+        ties++;
+    }
+}
+
+// this function checks if one of the players reached 5 points and resets the game
+function checkForWinner() {
+    if (playerScore == 5) {
+        displayText.textContent = "PLAYER WINS 5 ROUNDS. CONGRATULATIONS!";
+        startButton.textContent = "REPLAY?"
+        rockbtn.style.visibility = "hidden";
+        paperbtn.style.visibility = "hidden";
+        scissorsbtn.style.visibility = "hidden";
+    }
+    else if (computerScore == 5) {
+        displayText.textContent = "CPU WINS 5 ROUNDS. YOU LOSE!";
+        startButton.textContent = "REPLAY?"
+        rockbtn.style.display = "hidden";
+        paperbtn.style.display = "hidden";
+        scissorsbtn.style.display = "hidden";
+    }
+
+}
+
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+    ties = 0;
+    cpuDisplay.textContent = computerScore;
+    playerDisplay.textContent = playerScore;
+    startButton.textContent = "New Game";
+    displayText.textContent = "Click Rock, Paper or Scissors";
+    rockbtn.style.visibility = "visible";
+    paperbtn.style.visibility = "visible";
+    scissorsbtn.style.visibility = "visible";
+}
